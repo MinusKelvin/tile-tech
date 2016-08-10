81bea230fdf48886533488b074c05987a174dd14
MinusKelvin <minuskelvin.carlson@gmail.com>
2016-08-10 17:12:19 +1000

    Fixed shadow ache problem

--- src/minusk/tiletech/render/GLHandler.java
+++ src/minusk/tiletech/render/GLHandler.java
@@ -240,7 +240,7 @@ public class GLHandler {
 			glBindTexture(GL_TEXTURE_2D, shadowTex[i]);
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT32F, shadowmapSize, shadowmapSize, 0, GL_DEPTH_COMPONENT, GL_FLOAT, 0);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
